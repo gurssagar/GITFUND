8e57d9cbf44fa30aa72589cba4a40a3672dcbf84
gurssagar <123202956+gurssagar@users.noreply.github.com>
2025-04-08 11:57:10 +0530

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   providers: [GitHub({
     clientId: process.env.AUTH_GITHUB_ID,
     clientSecret: process.env.AUTH_GITHUB_SECRET,
