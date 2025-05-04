438c7e61a72ea59e03042ec434ee389f4913a5e8
gurssagar <123202956+gurssagar@users.noreply.github.com>
2025-05-04 11:28:33 +0530

    updated sidebar

--- src/assets/components/sidebar.tsx
+++ src/assets/components/sidebar.tsx
@@ -31,15 +31,23 @@ export default function Sidebar() {
                 </div>
                 <div className='pt-7'>
                     <div className='text-[13px] text-gray-400 py-2'>Contributor</div>
+                    <a className='/contributions'>
                     <div className='rounded-lg text-sm focus:bg-gray-400 hover:bg-gray-100 dark:hover:bg-[#27272a] px-2 py-2 flex'>
+                    
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" className='my-auto mr-2' height="20" viewBox="0 0 24 24">
                         <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 12h.009m3.986 0h.01m3.986 0H16m2 9c1.232 0 2.231-1.151 2.231-2.571c0-2.248-.1-3.742 1.442-5.52c.436-.502.436-1.316 0-1.818c-1.542-1.777-1.442-3.272-1.442-5.52C20.231 4.151 19.232 3 18 3M6 21c-1.232 0-2.231-1.151-2.231-2.571c0-2.248.1-3.742-1.442-5.52c-.436-.502-.436-1.316 0-1.818C3.835 9.353 3.769 7.84 3.769 5.57C3.769 4.151 4.768 3 6 3" color="currentColor"/></svg>Contributions</div>
-                    <div className='rounded-lg text-sm data-[active=true]:bg-gray-400 hover:bg-gray-100 dark:hover:bg-[#27272a] px-2 py-2 flex'><svg xmlns="http://www.w3.org/2000/svg" className='my-auto mr-2' width="20" height="20" viewBox="0 0 48 48"><defs><mask id="ipSSeoFolder0"><g fill="none" stroke-width="4"><path fill="#fff" stroke="#fff" stroke-linejoin="round" d="M5 8a2 2 0 0 1 2-2h12l5 6h17a2 2 0 0 1 2 2v26a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2z"/>
+                        </a>
+                        <a className='/projects'>
+                        <div className='rounded-lg text-sm data-[active=true]:bg-gray-400 hover:bg-gray-100 dark:hover:bg-[#27272a] px-2 py-2 flex'><svg xmlns="http://www.w3.org/2000/svg" className='my-auto mr-2' width="20" height="20" viewBox="0 0 48 48"><defs><mask id="ipSSeoFolder0"><g fill="none" stroke-width="4"><path fill="#fff" stroke="#fff" stroke-linejoin="round" d="M5 8a2 2 0 0 1 2-2h12l5 6h17a2 2 0 0 1 2 2v26a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2z"/>
+                    
                     <path stroke="#000" stroke-linecap="round" stroke-linejoin="round" d="m14 22l5 5l-5 5"/><path stroke="#000" stroke-linecap="round" d="M26 32h8"/></g></mask></defs>
+                    
                     <path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSSeoFolder0)"/></svg>Projects</div>
+                    </a>
+                    <a href="Rewards">
                     <div className='rounded-lg text-sm data-[active=true]:bg-gray-400 hover:bg-gray-100 dark:hover:bg-[#27272a] px-2 py-2 flex'><svg xmlns="http://www.w3.org/2000/svg" className='my-auto mr-2' width="20" height="20" viewBox="0 0 12 12">
                     <path fill="currentColor" d="M2.25 1C1.56 1 1 1.56 1 2.25v1.162a1.5 1.5 0 0 0 .772 1.31l2.876 1.599a3 3 0 1 0 2.704 0l2.877-1.598A1.5 1.5 0 0 0 11 3.412V2.25C11 1.56 10.44 1 9.75 1zM2 2.25A.25.25 0 0 1 2.25 2H4v2.817l-1.743-.968A.5.5 0 0 1 2 3.412zm3 3.122V2h2v3.372l-1 .556zm3-.555V2h1.75a.25.25 0 0 1 .25.25v1.162a.5.5 0 0 1-.257.437zM8 9a2 2 0 1 1-4 0a2 2 0 0 1 4 0"/></svg>Rewards</div>
-
+                    </a>
                 </div>
                 <div className='pt-7'>
                     <div className='text-[13px] text-gray-400 py-2'>Maintainer</div>
