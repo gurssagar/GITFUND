b8048056e9a2dbb3bea67be31b22c5df6c54eb11
Gursagar Singh <123202956+gurssagar@users.noreply.github.com>
2025-07-02 00:32:53 +0530

    colors updated

--- src/components/ui/glowing-effect.tsx
+++ src/components/ui/glowing-effect.tsx
@@ -144,15 +144,15 @@ const GlowingEffect = memo(
                   var(--black),
                   var(--black) calc(25% / var(--repeating-conic-gradient-times))
                 )`
-                  : `radial-gradient(circle, #dd7bbb 10%, #dd7bbb00 20%),
+                  : `radial-gradient(circle,rgb(123, 221, 200) 10%, #dd7bbb00 20%),
                 radial-gradient(circle at 40% 40%, #d79f1e 5%, #d79f1e00 15%),
-                radial-gradient(circle at 60% 60%, #5a922c 10%, #5a922c00 20%), 
+                radial-gradient(circle at 60% 60%,rgb(12, 237, 245) 10%, #5a922c00 20%), 
                 radial-gradient(circle at 40% 60%, #4c7894 10%, #4c789400 20%),
                 repeating-conic-gradient(
                   from 236.84deg at 50% 50%,
-                  #dd7bbb 0%,
+rgb(123, 221, 188) 0%,
                   #d79f1e calc(25% / var(--repeating-conic-gradient-times)),
-                  #5a922c calc(50% / var(--repeating-conic-gradient-times)), 
+rgb(10, 226, 255) calc(50% / var(--repeating-conic-gradient-times)), 
                   #4c7894 calc(75% / var(--repeating-conic-gradient-times)),
                   #dd7bbb calc(100% / var(--repeating-conic-gradient-times))
                 )`,
