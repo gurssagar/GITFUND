ff2e79d169860456569fdacaff02de638d1877a4
03-lovepreetSingh <lovepreetsingh9810573475@gmail.com>
2025-07-04 16:12:15 +0530

    updated

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -208,7 +208,7 @@ export default function Home() {
                   Discover projects that match the languages you love to code
                   in.
                 </p>
-                <div className="py-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-4 lg:gap-6">
                   {isLoading ? (
                     <>
                       {repoData.map((repo: any) => {
